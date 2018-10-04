b24674de63cec3b1804250675a3c77c3ec32b588
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-10-04 13:13:41 -0600

    Update

---
+++ Index.js
@@ -0,0 +1,11 @@
+//Good Morning
+let hour >= 07; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+let hour <= 10 || <11; 
+if (hour) console.log('Good Morning');
+
+//Good Afternoon
+let hour <= 12
+let hour < 18 || > 24;
+else if () console.log('Good Afternoon');
+else if () console.log('Good Afternoon');
+else console.console.log();
